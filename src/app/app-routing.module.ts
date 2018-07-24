f64fc3780e40507cb41adcfbfec0cad330a291d9
tanuja <tanuja@capitalnumbers.com>
2018-07-24 18:08:52 +0530

    Worked on the front page in angular and css

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule }             from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PersonalComponent }    from './personal/personal.component';
+import { WorkComponent }        from './work/work.component';
+import { AddressComponent }     from './address/address.component';
+import { ResultComponent }      from './result/result.component';
+
+import { WorkflowGuard }        from './workflow/workflow-guard.service';
+import { WorkflowService }      from './workflow/workflow.service';
+import { BoxSelectorComponent } from './box-selector/box-selector.component';
+
+export const appRoutes: Routes = [
+    // 1st Route
+    { path: 'app-box-selector',  component: BoxSelectorComponent },
+    // 1st Route
+    { path: 'personal',  component: PersonalComponent },
+    // 2nd Route
+    { path: 'work',  component: WorkComponent },
+    // 3rd Route
+    { path: 'address',  component: AddressComponent, canActivate: [WorkflowGuard] },
+    // 4th Route
+    { path: 'result',  component: ResultComponent, canActivate: [WorkflowGuard] },
+    // 5th Route
+    { path: '',   redirectTo: '/personal', pathMatch: 'full' },
+    // 6th Route
+    { path: '**', component: BoxSelectorComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true} )],
+  exports: [RouterModule],
+  providers: [WorkflowGuard]
+})
+
+export class AppRoutingModule {}
---
+++ src/app/box-selector/box-selector.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }              from '@angular/router';
+
+import { BoxSelector }            from '../data/formData.model';
+import { FormDataService }     from '../data/formData.service';
+
+@Component({
+  selector: 'app-box-selector',
+  templateUrl: './box-selector.component.html',
+  styleUrls: ['./box-selector.component.css']
+})
+export class BoxSelectorComponent implements OnInit {
+  title : "Step 1 of 4 ";
+  box_selector:BoxSelector;
+  form: any;
+  constructor(private router: Router, private formDataService: FormDataService) { }
+
+  ngOnInit() {
+         this.box_selector = this.formDataService.getBoxSelector();
+         console.log('BoxSelector feature loaded!')
+  }
+  
+    goToNext(form: any) {
+        this.router.navigate(['/work']);
+    }
+
+}
